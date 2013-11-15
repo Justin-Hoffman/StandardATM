0f7350590c03fbb75e73d69108df0ae8cf09b4e7
Perfexion <itgjimmy@gmail.com>
2013-11-15 15:14:03 -0700

    Update standardatm.m
    Added viscosity

--- standardatm.m
+++ standardatm.m
@@ -9,6 +9,7 @@
     %    air.rho   - Density
     %    air.a     - Speed of Sound
     %    air.h     - Altitude
+    %    air.mu    - Dynamic Viscosity
     %
     %    air.Tsl   - Sea Level Temperature
     %    air.Psl   - Sea Level Pressure
@@ -61,6 +62,8 @@ function air = standardatm(h,varargin)
     Psl = 14.697*144;
     rhosl = 23.77*10^-4;
     gma = 1.4;
+    muinterp = [3.8360    3.7370    3.6370    3.5340    3.4300    3.3240    3.2170    3.1070    2.9950    2.9690    2.9690    2.9690    2.9690    2.9840 3.0180    3.0520    3.0870    3.5110    3.2790    2.8460]*10^-7;
+    hinterp = [ -5000           0        5000       10000       15000       20000       25000       30000       35000       40000       45000       50000 60000       70000       80000       90000      100000      150000      200000      250000];
     
     for i = 1:length(h)
         if h(i)<hb1
@@ -76,7 +79,10 @@ function air = standardatm(h,varargin)
         elseif h(i) <= hb6
             [T(i) P(i) rho(i) a(i)] = mesosphere2(h(i));
         end
+        mu(i) = interp1(hinterp,muinterp,h(i));
     end
+
+    
     %Assemble Structure
     air.T = T;
     air.Tsl = Tsl;
@@ -84,6 +90,7 @@ function air = standardatm(h,varargin)
     air.Psl = Psl;
     air.rho = rho;
     air.rhosl = rhosl;
+    air.mu = mu;
     air.a = sqrt(gma*R*air.T);
     air.sigma = air.rho/rhosl;
     air.delta = air.P/Psl;
@@ -101,6 +108,7 @@ function air = standardatm(h,varargin)
        air.a = air.a/3.28084;
        air.rho = air.rho*515.378818;
        air.rhosl = air.rhosl*515.378818;
+       air.mu = air.mu*4.448222/(0.092903);
        air.Ta = air.Ta*5/9*3.28084;
        air.R = 286.9;
        air.h = air.h/3.28084;
